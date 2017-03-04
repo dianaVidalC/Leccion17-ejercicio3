c4de28493326e1918ccc722ffb83b1a2813a29f7
Erika Vidal <erikadianavidalcapillo@gmail.com>
2017-03-04 18:57:19 -0500

    arreglando nombre y color

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function myFunction() {
+  var nombre=document.getElementById('name').value;
+  var mail=document.getElementById('email').value;
+  document.getElementById("mostrarNombre").innerHTML=nombre;
+  document.getElementById("mostrarMail").innerHTML=mail;
+}
